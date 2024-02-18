6a982fc8ee39f6ed5ec712872dbdd5dd8c94125e
Pavel <gordey8823@yandex.ru>
2024-02-18 11:16:47 +0500

    add code

---
+++ code/Program.cs
@@ -0,0 +1,37 @@
+﻿// Функция печатает массив
+void PrintArray(string[] array)
+{
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+}
+
+// Функция возвращает новый массив в котором элементы с количеством знаков меньше или равно трем 
+string[] FilterArray(string[] array)
+{
+    int arrayLength = array.Length;
+    int n = 0;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            n += 1;
+        }
+    }
+    string[] resultArray = new string[n];
+    int i2 = 0;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultArray[i2] = array[i];
+            i2 += 1;
+        }
+    }
+
+    return resultArray;
+}
+
+
+string[] newArray = { "Hello", "world" };
+PrintArray(newArray);
+PrintArray(FilterArray(newArray));
+
